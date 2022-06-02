16dcc2fceeedc85f340f9f031917658961becb83
milekawaii0666@gmail.com <jamile.isabel.silva.oficial@gmail.com>
2022-06-02 16:50:21 -0300

    versao_quase_completa

---
+++ App.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import { Entypo, Feather } from '@expo/vector-icons'
+
+import Login from './components/login';
+import Criar from './components/create';
+import Ler from './components/read';
+import Atualizar from './components/update';
+import NewUser from './components/newUser';
+import Home from './components/home';
+import Conta from './components/count';
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function Tabs({ route }) {
+  var param = route.params?.idUser
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#121212',
+          borderTopColor: 'tranparent',
+          paddingBottom: 5,
+          paddingTop: 5,
+        },
+        tabBarActiveTintColor: '#fff',
+        tabBarInactiveTintColor: '#555'
+      }}
+    >
+
+      <Tab.Screen name='Adiconar contatos' component={Criar} initialParams={{ 'iddocara': param }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="news" size={size} color={color} />
+          )
+        }}
+      />
+
+      <Tab.Screen name='Contatos' component={Ler} initialParams={{ 'iddocara': param }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="users" size={size} color={color} />
+          )
+        }}
+      />
+
+      <Tab.Screen name='Meu perfil' component={Conta} initialParams={{ 'iddocara': param }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Login'
+          component={Login}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name='NewUser'
+          component={NewUser}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Uptade'
+          component={Atualizar}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='TabBar'
+          component={Tabs} //Tabs = Chama toda a TabBar
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Count'
+          component={Conta}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Create'
+          component={Criar}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Read'
+          component={Ler}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  menuHome: {
+    display: 'none'
+  }
+})
+
---
+++ components/newUser.js
@@ -0,0 +1,178 @@
+import React, { useState, useEffect } from 'react'
+import {
+    StyleSheet, Text, View, TextInput,
+    TouchableOpacity, KeyboardAvoidingView, Platform
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import firebaseApp from './firebaseConfig'
+
+export default function NewUser({ navigation }) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
+    const [errorRegister, setErrorRegister] = useState('')
+
+    const register = () => {
+        if (password != password2){
+            alert('As senhas não coincidem')
+
+        }else{firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                let user = userCredential.user;
+                navigation.navigate('Home', { idUser: user.id })
+            })
+            .catch((error) => {
+              setErrorRegister(true)
+                let errorCode = error.code
+                let errorMessage = error.message
+            });}
+        
+    }
+
+    useEffect(() => { }, []);
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}
+        >
+            <View style={styles.register}>
+                <Text style={styles.title}>Novo usuario</Text>
+                <TextInput style={styles.textInput1}
+                    placeholder='Email'
+                    type='text'
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                />
+                <TextInput style={styles.textInput1}
+                    placeholder='Senha'
+                    secureTextEntry={true}
+                    type='password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                />
+                <TextInput style={styles.textInput1}
+                    placeholder='Confirma senha'
+                    secureTextEntry={true}
+                    type='password'
+                    onChangeText={(text) => setPassword2(text)}
+                    value={password2}
+                />
+                
+                {/*############ ERROR #######################*/}
+                {errorRegister === true
+                    ?
+                    <View style={styles.error}>
+                        <MaterialCommunityIcons
+                            name='alert-circle'
+                            size={20}
+                            color='#f00'
+                        /><Text style={styles.warning}>E-mail ou senha inválido...</Text>
+                    </View>
+                    :
+                    <View />
+                }
+                {/*############ FIM ERROR ##################*/}
+
+                {email === "" || password === ""
+                    ?
+                    <TouchableOpacity
+                        disabled={true}
+                        style={styles.buttonRegister}
+                    >
+                        <Text style={styles.textRegisterOff}>Cadastrar</Text>
+                    </TouchableOpacity>
+                    :
+                    <TouchableOpacity style={styles.buttonRegister}
+                        onPress={register}
+                    >
+                        <Text style={styles.textRegisterOn}>Cadastrar</Text>
+                    </TouchableOpacity>
+                }
+                <Text style={styles.registration}>
+                    Já possuí cadastro?
+                    <TouchableOpacity
+                        style={styles.linkSubscribe}
+                        onPress={()=>navigation.navigate('Login')}
+                    >
+                        <Text> Logar agora</Text>
+                    </TouchableOpacity>
+                </Text>
+
+            </View>
+
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ccc',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        color: '#f00',
+        fontSize: 20,
+    },
+    textInput1: {
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: '#eee',
+        borderRadius: 5,
+        borderColor: '#ccc',
+        marginBottom: 10,
+        marginTop: 10,
+        width: '90%',
+        height: 40,
+    },
+    register: {
+        alignItems: 'center',
+        width: '80%',
+        height: '70%',
+        backgroundColor: '#fff',
+        borderRadius: 15,
+    },
+    title: {
+        marginTop: 20,
+        marginBottom: 10,
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#0ff',
+    },
+    error: {
+        flexDirection: 'row',
+        width: '80%',
+    },
+    warning: {
+        flexDirection: 'row',
+        paddingLeft: 5,
+    },
+    buttonRegister: {
+        width: 90,
+        height: 45,
+        backgroundColor: '#54f0a7',
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+    textRegisterOff: {
+        fontSize: 16,
+        color: '#aaa',
+    },
+    textRegisterOn: {
+        fontSize: 16,
+        color: '#555',
+    },
+    registration: {
+        marginTop: 20,
+        fontWeight: 'italic',
+        fontSize: 12,
+
+    },
+    linkSubscribe: {
+        color: '#1256c4',
+    }
+});
